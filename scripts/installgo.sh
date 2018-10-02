8812d04dbd0384d32e261948f5500492fe17916f
Viral Patel <viral.patel@fourth.com>
2018-10-02 21:48:17 +0100

    ammending go script

--- scripts/installgo.sh
+++ scripts/installgo.sh
@@ -2,4 +2,8 @@ if [ ! -d "/etc/go" ]; then
     echo "Go Not Found In Path Downloading...."
     curl "https://dl.google.com/go/go1.11.1.linux-amd64.tar.gz" | tar xvz;
     sudo cp -R ./go /etc/;
+    export PATH=$PATH:/etc/go/bin
+    else
+        echo "Adding Go Path"
+        export PATH=$PATH:/etc/go/bin
 fi
